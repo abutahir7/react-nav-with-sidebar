04c590d13bca455eb36755706965eefd09fd8da2
Abu <abu@mudrex.com>
2020-06-16 00:35:03 +0530

    nav bar added

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Toolbar.css'
+const toolbar = props => (
+        <header className="toolbar">
+            <nav className="toolbar__navigation">
+                <div>
+
+                </div>
+                <div className="toolbar__logo"><a href="/">THE LOGO</a></div>
+                <div className="spacer"></div>
+                <div className="toolbar_navigation-items">
+                    <ul>
+                        <li><a href="/products">Products</a></li>
+                        <li><a href="/users">Users</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
+
+export default toolbar;
